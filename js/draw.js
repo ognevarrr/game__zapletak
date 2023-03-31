187b4cae441be1f3e21f74792e85f6cac5e6e680
ognevarrr <varchunya@gmail.com>
2023-03-31 04:15:48 +0300

    bh

--- js/draw.js
+++ js/draw.js
@@ -25,58 +25,14 @@ $(document).ready(function(){
 			rot3 += 45;
 			$(this).css("transform", `rotate(${rot3}deg)`)
 	});
-
-let
-    canvas, ctx, brush = {
-      x: 0,
-      y: 0,
-      size: 5,
-      color: var(--blue);
-      down: false,
-    },
-    strokes = [],
-    currentStroke = null;
-  function init () {
-    canvas = $('#draw');
-    canvas.attr({
-      width: window.innerWidth,
-      height: window.innerHeight,
-    });
-    ctx = canvas[0].getContext('2d');
-    function mouseEvent (e) {
-      brush.x = e.pageX;
-      brush.y = e.pageY;
-      currentStroke.points.push({
-        x: brush.x,
-        y: brush.y,
-      });
-    }
-    canvas.mousedown(function (e) {
-      brush.down = true;
-      currentStroke = {
-        color: brush.color,
-        size: brush.size,
-        points: [],
-      };
-      strokes.push(currentStroke);
-      mouseEvent(e);
-    }).mouseup(function (e) {
-        brush.down = false;
-        mouseEvent(e);
-      currentStroke = null;
-    }).mousemove(function (e) {
-      if (brush.down)
-      mouseEvent(e);
-    });
-    $('.c_yellow').click(function () {
-      brush.color = "yellow";
-    });
-    $('.c_orange').click(function () {
-      brush.color = "orange";
-    });
-    $('.c_black').click(function () {
-      brush.color = "black";
-    });
-  }
-  $(init);
 });
+
+
+const canvas = document.querySelector('canvas')
+ctx = canvas.getContext("2d");
+
+const drawing = (e) => {
+  ctx.lineTo( e.offsetX, e.offsetY);
+  ctx.stroke();
+}
+canvas.addEventListener("mousemove", drawing);
