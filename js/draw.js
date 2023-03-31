f518deb49549b2e86e3d2bbef33105642ee998fc
ognevarrr <varchunya@gmail.com>
2023-03-31 03:48:55 +0300

    opa

--- js/draw.js
+++ js/draw.js
@@ -25,4 +25,57 @@ $(document).ready(function(){
 			rot3 += 45;
 			$(this).css("transform", `rotate(${rot3}deg)`)
 	});
+
+let
+    canvas, ctx, brush = {
+      x: 0,
+      y: 0,
+      size: 5,
+      down: false,
+    },
+    strokes = [],
+    currentStroke = null;
+  function init () {
+    canvas = $('#draw');
+    canvas.attr({
+      width: window.innerWidth,
+      height: window.innerHeight,
+    });
+    ctx = canvas[0].getContext('2d');
+    function mouseEvent (e) {
+      brush.x = e.pageX;
+      brush.y = e.pageY;
+      currentStroke.points.push({
+        x: brush.x,
+        y: brush.y,
+      });
+    }
+    canvas.mousedown(function (e) {
+      brush.down = true;
+      currentStroke = {
+        color: brush.color,
+        size: brush.size,
+        points: [],
+      };
+      strokes.push(currentStroke);
+      mouseEvent(e);
+    }).mouseup(function (e) {
+        brush.down = false;
+        mouseEvent(e);
+      currentStroke = null;
+    }).mousemove(function (e) {
+      if (brush.down)
+      mouseEvent(e);
+    });
+    $('.c_yellow').click(function () {
+      brush.color = "yellow";
+    });
+    $('.cb02').click(function () {
+      brush.color = "black";
+    });
+    $('.cb03').click(function () {
+      brush.color = "black";
+    });
+  }
+  $(init);
 });
